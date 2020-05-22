84322670d4873b74f65d8176c8558acaded1ea5e
amit-indorz <amit@indorz.co>
2020-05-22 16:04:15 +0300

    add all neseccery consts variables for deploy

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,15 +1,14 @@
-const config = require('config');
-const xoauth2 = require('xoauth2');
-
 module.exports = Object.freeze({
   SMTP: {
-    service: config.get('smtp.service'),
+    service: 'gmail',
     auth: {
       type: 'OAuth2',
-      user: config.get('smtp.user'),
-      clientId: config.get('smtp.clientId'),
-      clientSecret: config.get('smtp.clientSecret'),
-      refreshToken: config.get('smtp.refreshToken'),
+      user: 'amitdanonmail@gmail.com',
+      clientId:
+        '273343245441-um6vgnk5lu44ba68cf42pthq3nu5n349.apps.googleusercontent.com',
+      clientSecret: 'v0KmgWqkBdxvILPq3m8qA0-b',
+      refreshToken:
+        '1//04PYk8Ja1MO5XCgYIARAAGAQSNwF-L9Ir2o5souzo-w-lCvxIEobzM879WEV5DpQZ0AGBdKum36u7Aj4pAL51LPV_F9nNBiusTic',
     },
   },
   HTTP_STATUS: {
@@ -24,4 +23,7 @@ module.exports = Object.freeze({
   EXPIRIATION_TIME: 60 * 60,
   SALT_ROUNDS: 10,
   TOKEN_SECRET: 'amitSecretKey',
+  DB_CONNECT:
+    'mongodb+srv://ipodhii:Israel1234!!@cluster0-sdcnk.mongodb.net/test?retryWrites=true&w=majority',
+  PORT: '5000',
 });
