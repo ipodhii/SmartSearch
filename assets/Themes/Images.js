92387a2fb1d853a149a39935a4e78a2d74616c8d
amit-indorz <amit@indorz.co>
2020-05-31 22:55:12 +0300

    repleaced images in app

--- assets/Themes/Images.js
+++ assets/Themes/Images.js
@@ -1,12 +1,12 @@
 // leave off @2x/@3x
 
-
 const images = {
-//    back: require('../Images/back.png'),
-    back: require('../images/back.png'),
-    contactMem: require('../images/contactMem.png'),
-    menu: require('../images/menu.png'),
+  //    back: require('../Images/back.png'),
+  back: require('../images/back.png'),
+  contactMem: require('../images/contactMem.png'),
+  menu: require('../images/menu.png'),
+  fullStart: require('../images/priorityH.png'),
+  emptyStart: require('../images/priorityL.png'),
+};
 
-}
-
-export default images
+export default images;
